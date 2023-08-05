fc13d37ce4b8e8e04ffff7c0cfba52e4cbecb544
RJ Villanueva <rj.villanueva89@gmail.com>
2023-08-05 09:20:26 +0800

    minor adjustment to api/users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,13 @@
 import { db } from "@/database/db"
 import { users } from "@/database/schema/users"
+import { sql } from "drizzle-orm"
 import { NextResponse } from "next/server"
 
 export async function GET() {
-  const result = await db.select().from(users)
+  const result = await db
+    .select()
+    .from(users)
+    .orderBy(sql`${users.created_at} asc`)
 
   return NextResponse.json(result)
 }
